2ad6d7e17788a23678634e76f59064759a20fc64
arun <arunqs3375@gmail.com>
2026-03-25 20:30:19 +0530

    modified some file and added new file

--- EstablishingConnectionType4Driver.java
+++ EstablishingConnectionType4Driver.java
@@ -14,3 +14,92 @@ public class EstablishingConnectionType4Driver {
 		}
 	}
 }
+
+
+/*
+ After establishing connection
+ 
+ 
+ below methods are from Connection interface if we want to access these methods we need to import java.sql.Connection
+ 
+ methods:
+ 1)public abstract Statement createStatemet() throws SQLEXception
+ 2)public abstract PreparedStatement prepareStatement() throws SQLEXception
+ 3)public abstract CallableStatement prepareCall() throws SQLEXception
+
+ 
+ NOTE:
+ --------
+ -all JDBC interfaces are implemented by JDBC driver vendors in JDBC driver software
+ 
+ 
+ 
+ Statement interface:
+ ---------------
+ -used to execute static SQL queries.
+ 
+ Examles:
+ ---------
+ 1)insert into student values(1,"aaa",83)
+ 2)select * from student  where rollno=1;
+ 
+ 
+ 
+ 
+ -------------------------------------------------------------
+ 
+ peparedStatement interface
+ -------------------------
+ -it is used to execute dynamic SQL queries
+ 
+ examples:
+ ------------
+ 1)insert into student values(?,?,?)
+ 2)select * from student where rollno=?
+ 
+ -------------------------------------------------------------
+ 
+ CallableStatemnt interface:
+ -----------------------------
+ -used to execute PL/SQL programs
+ 
+ examples:
+ ---------
+ 1)Procedures
+ 2)Functions
+ --------------------------------------------------------------
+ 
+ Methods:
+ ----------
+ 1)public abstract boolean execute(String) throws SQLException
+ -the above ethod is used to exeute non select sql queries
+ -it is suitable to execute DDL queries
+ -DDL stands for data definition language
+ 
+ 2)public abstract int executeUpdate(String) throws SQLException
+ -the above method used to execute non select sql queries
+ -it is suitable to execute DML queries
+ -DML stands for data manipulation language
+ 
+ 3)public abstract ResultSet executeQuery(String) throws SQLException
+-it is used to execute select queries
+-suitable to execute DQL queries 
+-DQL stands for data query language. 
+ 
+ 
+ */
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Path setting for all JDK tools.md
@@ -0,0 +1,22 @@
+1)Type environment variables in search bar
+2)click on edit the environment variables
+
+one dailog box will pop up :
+
+3)click on advanced tab
+4)click on environment variables button
+5)if the path variale already exist then select the path variable then click on edit button
+6)click on new new button
+7)type the fllowing:
+8)C:\Program Files\Java\jdk-23\bin
+9)click on ok buttons.
+if the path variable does not exist then follow the following statements:
+1)click on new button
+2)type variable name path
+3)type variable value
+C:\Program Files\Java\jdk-23
+4)click on ok buttons.
+
+
+NOTE:
+user variables are specific to user here as system variables are common to all users
